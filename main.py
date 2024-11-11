1a11f71dc56adff791fb0766408533b1166b154c
Julian <julianilamo@hotmail.com>
2024-11-11 07:00:40 -0500

    model evaluation and pipeline creation for model evaluation, also created the prediction pipeline

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-#from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -54,7 +54,7 @@ except Exception as e:
 
 
 
-"""
+
 
 STAGE_NAME = "Model evaluation stage"
 try:
@@ -65,5 +65,3 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-        
-        """

720ea2f095d2351c83f2ac7d09cc5fd0cbdb4f3d
Julian <julianilamo@hotmail.com>
2024-11-11 05:48:45 -0500

    data validation research and pipeline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 #from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -16,7 +16,7 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-"""
+
 STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -28,7 +28,7 @@ except Exception as e:
         raise e
 
 
-
+"""
 STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
